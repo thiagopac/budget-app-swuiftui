4b7351e6cec88746bf526869325e58c4ca14d9d1
Thiago Pires <thiagopac@gmail.com>
2026-02-19 15:03:08 -0300

    Initial Commit

---
+++ BudgetsApp/BudgetsAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  BudgetsAppApp.swift
+//  BudgetsApp
+//
+//  Created by Thiago Castro on 19/02/26.
+//
+
+
+import SwiftUI
+
+@main
+struct BudgetsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
